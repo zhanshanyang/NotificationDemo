f93536b227fd46706eaedd5c6ef0464e1cd7692e
yangzhanshan <yangzhanshan@chehejia.com>
2019-07-18 14:56:30 +0800

    [notification] add foreground notification
    Signed-off-by: yangzhanshan <yangzhanshan@chehejia.com>

--- app/src/main/java/com/yzs/demo/notificationdemo/service/ForegroundServiceDemo.java
+++ app/src/main/java/com/yzs/demo/notificationdemo/service/ForegroundServiceDemo.java
@@ -5,10 +5,16 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
+import com.yzs.demo.notificationdemo.R;
+
 public class ForegroundServiceDemo extends Service {
+    public static final String EXTRAS_NOTIFICATION_SHOW = "android.chj.notification.show";
+    public static final int DEFAULT = 0;    // 不隐藏(默认)
+    public static final int NO_SHOW = 1;    // 隐藏
 
     private NotificationManager mNotificationManager;
     @Override
@@ -22,9 +28,16 @@ public class ForegroundServiceDemo extends Service {
         String channelId = "service_channel_id";
         NotificationChannel notificationChannel = new NotificationChannel(channelId,
                 "foreground des",
-                NotificationManager.IMPORTANCE_DEFAULT);
+                NotificationManager.IMPORTANCE_HIGH);
         mNotificationManager.createNotificationChannel(notificationChannel);
-        Notification notification = new Notification.Builder(this, channelId).build();
+        // put
+        Bundle extras = new Bundle();
+        extras.putInt(EXTRAS_NOTIFICATION_SHOW, NO_SHOW);
+        Notification notification = new Notification.Builder(this, channelId)
+                .setSmallIcon(R.drawable.jd_icon)
+                .setContentTitle("yang")
+                .addExtras(extras)
+                .build();
         startForeground(1, notification);
         return super.onStartCommand(intent, flags, startId);
     }
